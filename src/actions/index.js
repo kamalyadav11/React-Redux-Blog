658bfd12d22d5c1aa4832330296d9730d4ac6886
kamal11 <kyzx11@gmail.com>
2018-09-28 19:18:51 +0530

    Fixed posts url in action

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,7 @@ const ROOT_URL = "http://reduxblog.herokuapp.com/api";
 const key = "?key=kamal";
 
 export const fetchposts = () => {
-  const request = axios.get(`${ROOT_URL}/${key}`);
+  const request = axios.get(`${ROOT_URL}/posts${key}`);
 
   return {
     type: FETCH_POSTS,
