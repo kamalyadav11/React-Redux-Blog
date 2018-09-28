552970969c14519c653f635f12e80d56008a316d
kamal11 <kyzx11@gmail.com>
2018-09-28 23:04:00 +0530

    installed form reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form";
+
 import posts from "./postReducer";
 
 export default combineReducers({
-  posts
+  posts,
+  form: formReducer
 });
